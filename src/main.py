ef26d2f725283adf95d030034855394175e0cd62
wilantury <ventas@antury.net>
2020-02-15 10:14:18 -0500

    Implementación función generate_password()

--- src/main.py
+++ src/main.py
@@ -1,11 +1,38 @@
 # Resolve the problem!!
 import string
+import random
 
 SYMBOLS = list('!"#$%&\'()*+,-./:;?@[]^_`{|}~')
+min_size = 8
+max_size = 16
+char_start = 33
+char_end = 126
+
+def getChar(type_char):
+    if type_char == 0:
+        return string.ascii_lowercase[random.randint(0, len(string.ascii_lowercase)-1)]
+    elif type_char == 1:
+        return string.ascii_uppercase[random.randint(0, len(string.ascii_uppercase)-1)]
+    elif type_char == 2:
+        return chr(random.randint(48,57))
+    elif type_char == 3:
+        return string.punctuation[random.randint(0, len(string.punctuation)-1)]    
 
 
 def generate_password():
     # Start coding here
+    size_password = random.randint(min_size,max_size)
+    password = []
+    options = random.randint(0,3)
+    for idx in range(0,size_password):
+        password.insert(0, getChar(options))
+        options += 1
+        if(options > 3):
+            options = 0
+    print(password)
+    secure_password = ''.join(password)
+    return secure_password
+
 
 
 def validate(password):
